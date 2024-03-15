ed11d82b6ca2affc9496c8818aeade2ea5c5fa48
Gulsher407 <125590042+Gulsher407@users.noreply.github.com>
2024-03-16 00:12:13 +0500

    Update Home.jsx

--- Porfolio/src/components/Home.jsx
+++ Porfolio/src/components/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="py-16 w-full bg-gradient-to-b h-full from-black via-black to-gray-800"
+      className=" w-full bg-gradient-to-b h-full from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="max-w-screen-lg py-6 mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
             I'm a Front-end Developer
